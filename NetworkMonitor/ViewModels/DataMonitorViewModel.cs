9e5c6210e0e8454e128da075380e5d4e1b6ce696
Athan Bonis <athancb@hotmail.com>
2020-02-03 21:46:33 +0200

    Project added

---
+++ NetworkMonitor/ViewModels/DataMonitorViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NetworkMonitor.Commands;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace NetworkMonitor.ViewModels
+{
+    public class DataMonitorViewModel : BaseViewModel
+    {
+        private double _dataUsed;
+        private ICommand _buttonCommand;
+        private ObservableCollection<NetworkInterface> _networkInterfaces;
+        private NetworkInterface _selectedInterface;
+
+        public double DataUsed
+        {
+            get => _dataUsed;
+            set
+            {
+                _dataUsed = value; 
+                OnPropertyChanged(nameof(DataUsed));
+            }
+        }
+
+        public NetworkInterface SelectedInterface
+        {
+            get => _selectedInterface;
+            set
+            {
+                _selectedInterface = value;
+                OnPropertyChanged(nameof(SelectedInterface));
+            }
+        }
+
+        public ObservableCollection<NetworkInterface> NetworkInterfaces
+        {
+            get => _networkInterfaces;
+            set
+            {
+                _networkInterfaces = value;
+                OnPropertyChanged(nameof(NetworkInterfaces));
+            }
+        }
+
+        public ICommand ButtonCommand
+        {
+            get
+            {
+               return _buttonCommand ?? (_buttonCommand = new CommandHandler(param => StartMonitoring(),true));
+            }
+            
+        }
+
+        public EventHandler MonitoringEventHandler { get; set; }
+
+        public DataMonitorViewModel()
+        {
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            NetworkInterfaces = new ObservableCollection<NetworkInterface>(networkInterfaces);
+            
+        }
+
+        public void StartMonitoring()
+        {
+            DataUsed = ConvertBytesToMegabytes(SelectedInterface.GetIPv4Statistics().BytesReceived);
+            
+            MonitoringEventHandler.Invoke(this, new EventArgs());
+            var timer = new Timer();
+            timer.Tick += TimerOnTick;
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            var received = ConvertBytesToMegabytes(SelectedInterface.GetIPv4Statistics().BytesReceived);
+            var sent = ConvertBytesToMegabytes(SelectedInterface.GetIPv4Statistics().BytesSent);
+            DataUsed = received + sent;
+            
+            MonitoringEventHandler.Invoke(this, new EventArgs());
+        }
+
+        static double ConvertBytesToMegabytes(long bytes)
+        {
+            var mbs = (bytes / 1024f) / 1024f;
+            return Math.Round(mbs, 2);
+        }
+    }
+}
---
+++ NetworkMonitor/Views/DataMonitorView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NetworkMonitor.ViewModels;
+using Brush = System.Drawing.Brush;
+using Color = System.Drawing.Color;
+
+namespace NetworkMonitor.Views
+{
+    /// <summary>
+    /// Interaction logic for DataMonitorView.xaml
+    /// </summary>
+    public partial class DataMonitorView : UserControl
+    {
+        public DataMonitorView()
+        {
+            InitializeComponent();
+            ((DataMonitorViewModel)DataContext).MonitoringEventHandler += MonitoringEventHandler;
+           
+        }
+
+        private void MonitoringEventHandler(object sender, EventArgs e)
+        {
+            Font font = new Font("Microsoft Sans Serif", 10, System.Drawing.FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brush = new SolidBrush(Color.White);
+            Bitmap bitmapText = new Bitmap(16, 16);
+            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+
+            IntPtr icon;
+
+            g.Clear(Color.Transparent);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(((DataMonitorViewModel)DataContext).DataUsed.ToString("##.##"), font, brush, 0,0 );
+            icon = (bitmapText.GetHicon());
+            myNotifyIcon.Icon = Icon.FromHandle(icon);
+        }
+    }
+}
